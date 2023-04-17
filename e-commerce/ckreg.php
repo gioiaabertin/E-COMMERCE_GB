35cd35a9c76e86182c36f749cfb0002cc62acdbd
gioiaabertin <118894495+gioiaabertin@users.noreply.github.com>
2023-04-17 22:28:58 +0200

    registrati
    non va

--- e-commerce/ckreg.php
+++ e-commerce/ckreg.php
@@ -7,9 +7,9 @@ if ($_POST["this_pw"] == $_POST["this_pw2"]) {
 
     $_SESSION["this_u"] = $_POST["this_user"];
     $sql = "INSERT INTO utenti(nome,cognome,user,mail,cell,pw,ncarta,scadenzac)";
-    $sql = $sql . "VALUES (?,?,?,?,?,?,?)";
+    $sql = $sql . "VALUES (?,?,?,?,?,?,?,?)";
 
-    $stmt->bind_param("ss",$_POST["this_user"], md5($pw) );
+    $stmt->bind_param("ssssisis",$_POST["this_nome"], $_POST["this_cognome"],$_POST["this_user"],$_POST["this_mail"],$_POST["this_cell"],md5($pw),$_POST["this_ncarta"] );
     $stmt->execute();
     $stmt->store_result();
     $result =$stmt->get_result();
@@ -17,7 +17,7 @@ if ($_POST["this_pw"] == $_POST["this_pw2"]) {
 if (!$conn->query($sql)) {
     $msg = "inserimento NON avvenuto!"; //sistemato la logica (la tua era ridondante nelle chiamate)
 
-    header("location: home.php" . ($msg == "" ? "" : "?msg=$msg"));
+    header("location: index.php" . ($msg == "" ? "" : "?msg=$msg"));
 } else
     header("location: index.php");
 
